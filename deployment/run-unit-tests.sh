8da2e9ee245c51abeef7c971c69f15717cb8ec2b
Joe SHI <qiaoshi@amazon.com>
2020-04-05 20:56:26 +0800

    remove unit tests

--- deployment/run-unit-tests.sh
+++ deployment/run-unit-tests.sh
@@ -8,29 +8,5 @@
 #
 
 # Get reference for all important folders
-template_dir="$PWD"
-source_dir="$template_dir/../source"
-
-echo "------------------------------------------------------------------------------"
-echo "[Init] Clean old dist and node_modules folders"
-echo "------------------------------------------------------------------------------"
-echo "find $source_dir/services -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/services -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find $source_dir/services -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/services -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find ../ -type f -name 'package-lock.json' -delete"
-find $source_dir/services -type f -name 'package-lock.json' -delete
-echo "find $source_dir/resources -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/resources -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find $source_dir/resources -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/resources -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find ../ -type f -name 'package-lock.json' -delete"
-find $source_dir/resources -type f -name 'package-lock.json' -delete
-echo "find $source_dir/simulator -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/simulator -iname "node_modules" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find $source_dir/simulator -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null"
-find $source_dir/simulator -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null
-echo "find ../ -type f -name 'package-lock.json' -delete"
-find $source_dir/simulator -type f -name 'package-lock.json' -delete
-
+echo "no unit tests"
 
