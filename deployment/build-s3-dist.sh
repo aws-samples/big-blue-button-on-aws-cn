7193d36913993f4c43fd2bf9147b4856aa0c7c21
Joe SHI <qiaoshi@amazon.com>
2020-04-11 22:00:01 +0800

    using new build-s3-dist.sh

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
-#
-# This assumes all of the OS-level configuration has been completed and git repo has already been cloned
 #
 # This script should be run from the repo's deployment directory
 # cd deployment
-# ./build-s3-dist.sh source-bucket-base-name solution-name version-code
+# ./build-s3-dist.sh source-bucket-base-name solution-name version-code template-bucket-name
 #
-# Paramenters:
+# Parameters:
 #  - source-bucket-base-name: Name for the S3 bucket location where the template will source the Lambda
 #    code from. The template will append '-[region_name]' to this bucket name.
 #    For example: ./build-s3-dist.sh solutions my-solution v1.0.0
@@ -15,11 +12,13 @@
 #  - solution-name: name of the solution for consistency
 #
 #  - version-code: version of the package
+#
+#  - template-bucket-name: Name for S3 bucket location where the template will be located in.
 
 # Check to see if input has been provided:
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
     echo "Please provide the base source bucket name, trademark approved solution name and version where the lambda code will eventually reside."
-    echo "For example: ./build-s3-dist.sh solutions trademarked-solution-name v1.0.0"
+    echo "For example: ./build-s3-dist.sh solutions trademarked-solution-name v1.0.0 solutions-reference"
     exit 1
 fi
 
@@ -50,33 +49,45 @@ echo "copy yaml templates and rename"
 cp $template_dir/*.yaml $template_dist_dir/
 cd $template_dist_dir
 # Rename all *.yaml to *.template
-for f in *.yaml; do 
+for f in *.yaml; do
     mv -- "$f" "${f%.yaml}.template"
 done
 
 cd ..
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Updating code source bucket in template with $1"
+
     replace="s/%%BUCKET_NAME%%/$1/g"
     echo "sed -i '' -e $replace $template_dist_dir/*.template"
     sed -i '' -e $replace $template_dist_dir/*.template
+
     replace="s/%%SOLUTION_NAME%%/$2/g"
     echo "sed -i '' -e $replace $template_dist_dir/*.template"
     sed -i '' -e $replace $template_dist_dir/*.template
+
     replace="s/%%VERSION%%/$3/g"
+    echo "sed -i '' -e $replace $template_dist_dir/*.template"
+    sed -i '' -e $replace $template_dist_dir/*.template
+
+    replace="s/%%TEMPLATE_BUCKET_NAME%%/$4/g"
     echo "sed -i '' -e $replace $template_dist_dir/*.template"
     sed -i '' -e $replace $template_dist_dir/*.template
 else
     echo "Updating code source bucket in template with $1"
+
     replace="s/%%BUCKET_NAME%%/$1/g"
     echo "sed -i -e $replace $template_dist_dir/*.template"
     sed -i -e $replace $template_dist_dir/*.template
+
     replace="s/%%SOLUTION_NAME%%/$2/g"
     echo "sed -i -e $replace $template_dist_dir/*.template"
     sed -i -e $replace $template_dist_dir/*.template
+
     replace="s/%%VERSION%%/$3/g"
     echo "sed -i -e $replace $template_dist_dir/*.template"
     sed -i -e $replace $template_dist_dir/*.template
+
+    replace="s/%%TEMPLATE_BUCKET_NAME%%/$4/g"
+    echo "sed -i -e $replace $template_dist_dir/*.template"
+    sed -i -e $replace $template_dist_dir/*.template
 fi
-
-
